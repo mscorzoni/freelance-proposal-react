578ae06e892231c788d173c9ca4fd93a01f907bb
Marcos Scorzoni <mlessa@gmail.com>
2018-10-26 16:15:06 +0100

    test

--- client/src/components/UI/Button/Button.js
+++ client/src/components/UI/Button/Button.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import classes from './Button.css';
 
 const button = (props) => (
   <button
-    className={[classes.Button, classes[props.btnType]].join(' ')}
+    className={classes.Success}
     onClick={props.clicked}
     >
     {props.children}
